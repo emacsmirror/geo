d7bd8e778ee16524f3eed3a6143a1751a58be5a1
oldosfan <luangruo@yahoo.com>
2020-05-28 18:22:26 +0800

    Implement `resolve-geocode-place'

--- geocode-glib/src/geocode-emacs.c
+++ geocode-glib/src/geocode-emacs.c
@@ -7,11 +7,17 @@
 #include "emacs-module.h"
 
 #include <stdio.h>
+#include <string.h>
 
 #include <geocode-glib/geocode-glib.h>
 
 #define Qnil(env) ((env)->intern ((env), "nil"))
 #define Qt(env) ((env)->intern ((env), "t"))
+#define Q(str, env) ((env)->intern ((env), str))
+#define S(str, env) ((str) ? (env)->make_string ((env), (str), strlen (str)) : Qnil (env))
+
+#define XGEOCODE_LOCATION(loc, env) \
+  ((GeocodeLocation *) (env)->get_user_ptr ((env), (loc)))
 
 void
 plugin_is_GPL_compatible ()
@@ -22,6 +28,14 @@ static void
 geocode_location_finalizer (void *value)
 {
   g_object_unref (value);
+}
+
+static emacs_value
+lisp_cons (emacs_env *env,
+	   emacs_value a,
+	   emacs_value b)
+{
+  return env->funcall (env, Q ("cons", env), 2, (emacs_value []) {a, b});
 }
 
 static emacs_value
@@ -37,6 +51,50 @@ make_geocode_location (emacs_env *env,
 
   return env->make_user_ptr (env, geocode_location_finalizer,
 			     geocode_location_new (lat, lon, acc));
+}
+
+static inline emacs_value
+geocode_place_to_emacs (GeocodePlace *place, emacs_env *env)
+{
+  emacs_value lisp_place = Qnil (env);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("street", env),
+				     S (geocode_place_get_street (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("town", env),
+				     S (geocode_place_get_town (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("state", env),
+				     S (geocode_place_get_state (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("country", env),
+				     S (geocode_place_get_country (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("county", env),
+				     S (geocode_place_get_county (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("continent", env),
+				     S (geocode_place_get_continent (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env,
+			  lisp_cons (env, Q ("street-address", env),
+				     S (geocode_place_get_street_address (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env, lisp_cons (env, Q ("building", env),
+					  S (geocode_place_get_building (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env, lisp_cons (env, Q ("postal-code", env),
+					  S (geocode_place_get_postal_code (place), env)),
+			  lisp_place);
+  lisp_place = lisp_cons (env, lisp_cons (env, Q ("country-code", env),
+					  S (geocode_place_get_postal_code (place), env)),
+			  lisp_place);
+  return lisp_place;
 }
 
 static emacs_value
@@ -69,6 +127,60 @@ Fgeocode_location_p (emacs_env *env,
   return geocode_location_p (env, args[0]);
 }
 
+static void
+Gresolve_geocode_place_cb (GObject *so,
+			   GAsyncResult *res,
+			   gpointer user_data)
+{
+  emacs_env *env = ((void **) user_data)[0];
+  emacs_value *vl = ((void **) user_data)[1];
+  GError **e = ((void **) user_data)[2];
+
+  GeocodePlace *place =
+    geocode_reverse_resolve_finish (GEOCODE_REVERSE (so),
+				    res, e);
+
+  if (place)
+    *vl = geocode_place_to_emacs (place, env);
+  g_object_unref (place);
+}
+
+static emacs_value
+resolve_geocode_place (emacs_env *env,
+		       emacs_value geocode_location)
+{
+  if (!geocode_location_p (env, geocode_location))
+    env->non_local_exit_signal (env,
+				Q ("wrong-type-argument", env),
+				S ("geocode-location-p", env));
+  GeocodeLocation *gl = XGEOCODE_LOCATION (geocode_location, env);
+  GeocodeReverse *rev = geocode_reverse_new_for_location (gl);
+  emacs_value val = NULL;
+  GError *error = NULL;
+  geocode_reverse_resolve_async (rev, NULL, Gresolve_geocode_place_cb,
+				 ((void *[]) {env, &val, &error}));
+
+  while (!val && !error)
+    g_main_context_iteration (NULL, false);
+  if (error)
+    {
+      g_error_free (error);
+      env->non_local_exit_signal (env, Q ("error", env),
+				  S ("Error retrieving location", env));
+    }
+  g_object_unref (rev);
+  return val;
+}
+
+static emacs_value
+Fresolve_geocode_place (emacs_env *env,
+			ptrdiff_t nargs,
+			emacs_value *args,
+			void *data)
+{
+  return resolve_geocode_place (env, args[0]);
+}
+
 extern int
 emacs_module_init (struct emacs_runtime *runtime)
 {
@@ -86,13 +198,27 @@ usage: (make-geocode-location LAT LON ACCURACY)", NULL);
 geocode location.\n\
 usage: (geocode-location-p LOCATION)", NULL);
 
+  emacs_value resolve_geocode_place
+    = env->make_function (env, 1, 1, Fresolve_geocode_place,
+			  "Resolve reverse geocode information \
+from LOCATION.\n\
+usage: (resolve-geocode-place LOCATION)", NULL);
+
   emacs_value Qmake_geocode_location
     = env->intern (env, "make-geocode-location");
   emacs_value Qgeocode_location_p
     = env->intern (env, "geocode-location-p");
+  emacs_value Qresolve_geocode_place
+    = env->intern (env, "resolve-geocode-place");
   env->funcall (env, env->intern (env, "defalias"), 2,
 		(emacs_value []) {Qmake_geocode_location, make_geocode_location});
   env->funcall (env, env->intern (env, "defalias"), 2,
 		(emacs_value []) {Qgeocode_location_p, geocode_location_p});
+
+  env->funcall (env, env->intern (env, "defalias"), 2,
+		(emacs_value []) {Qresolve_geocode_place, resolve_geocode_place});
+
+  env->funcall (env, env->intern (env, "provide"), 1,
+		(emacs_value []) {env->intern (env, "geocode-emacs")});
   return 0;
 }
